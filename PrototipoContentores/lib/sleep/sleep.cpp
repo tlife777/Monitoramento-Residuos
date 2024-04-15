3becccfe7c771ae6bb440b631a02fa7cf9d2bd01
KaiqueVA <kaique_x_@hotmail.com>
2024-04-15 15:41:44 -0300

    rework2

--- PrototipoContentores/lib/sleep/sleep.cpp
+++
@@ -1,9 +0,0 @@
-#include "sleep.h"
-
-void espSleep(unsigned int secondsSleep)
-{
-  esp_sleep_enable_timer_wakeup(secondsSleep * uS_TO_S_FACTOR);
-  //esp_sleep_enable_ext0_wakeup(GPIO_NUM_34, 1); //1 = High, 0 = Low
-  Serial.println("Dormindo...");
-  esp_deep_sleep_start();
-}
