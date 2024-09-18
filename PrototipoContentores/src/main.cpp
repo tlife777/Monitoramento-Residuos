fa8f626caf57c5dab0fec965e8a86d68d24b9223
tlife <111544734+tlife777@users.noreply.github.com>
2024-09-18 14:50:20 -0300

    fix: renamed the functions calls in main file

--- PrototipoContentores/src/main.cpp
+++ PrototipoContentores/src/main.cpp
@@ -7,10 +7,10 @@ TinyGPSPlus gps;    // Inicializa o módulo GPS
 VL53L0X sensor;     // Inicializa o sensor de distância a laser
 
 // Declaração de funções (protótipos) para que o compilador saiba que elas existem antes de serem chamadas
-void init_sensors();
-void init_lora();
-void coletar_dados(char** dados, uint16_t* tamanhoStr);
-void enviarDadosLoRa(const char* dados);
+void initSensors();
+void initLora();
+void dataCollector(char** dados, uint16_t* tamanhoStr);
+void sendLoraPayload(const char* dados);
 
 void setup() {
     Serial.begin(BAUDRATE_SERIAL_DEBUG);  // Inicializa o monitor serial
@@ -18,11 +18,11 @@ void setup() {
     Serial.println("Iniciando o setup..."); // Adiciona log para depuração
 
     // Inicializa os sensores
-    init_sensors();
+    initSensors();
     Serial.println("Sensores inicializados."); // Adiciona log para depuração
 
     // Inicializa o módulo LoRa
-    init_lora();
+    initLora();
     Serial.println("LoRa inicializado."); // Adiciona log para depuração
 }
 
@@ -31,11 +31,11 @@ void loop() {
     uint16_t tamanhoStr = 0;
 
     // Coleta e processa dados
-    coletar_dados(&dados, &tamanhoStr);
+    dataCollector(&dados, &tamanhoStr);
 
     // Verifica se os dados são válidos antes de enviar
     if (dados != nullptr) {
-        enviarDadosLoRa(dados);
+        sendLoraPayload(dados);
         free(dados); // Libera a memória alocada
     } else {
         Serial.println("Esperando por dados de GPS válidos...");
